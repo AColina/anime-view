76845d9b6688cf9ccf793b2cb026535edc6c9821
angel <angelcolina13@gmail.com>
2017-11-01 20:41:20 -0400

    implementacion de swagger

---
+++ src/main/java/com/acolina/animeview/config/SwaggerConfig.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2017 Pivotal Software, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.acolina.animeview.config;
+
+import java.util.ArrayList;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author angel
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.acolina.animeview"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot REST API",
+                "Spring Boot REST API for Online Store",
+                "1.0",
+                "Terms of service",
+                new Contact("Angel Colina", "", "angelcolina13@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
+        return apiInfo;
+    }
+}
--- src/main/java/com/acolina/animeview/controller/AppController.java
+++ src/main/java/com/acolina/animeview/controller/AppController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author angel
  */
-@RestController
+//@RestController
 public class AppController {
 
     @RequestMapping(method = RequestMethod.GET)
